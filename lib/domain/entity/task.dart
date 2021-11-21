a783a553d764157d300cf56d93c3a17a73429a2b
anega <anegamega@gmail.com>
2021-11-21 13:32:32 +0200

    Add Task entity and Hive generator.

---
+++ lib/domain/entity/task.dart
@@ -0,0 +1,16 @@
+import 'package:hive/hive.dart';
+
+part 'task.g.dart';
+
+@HiveType(typeId: 2)
+class Task {
+  @HiveField(0)
+  String text;
+  @HiveField(1)
+  bool done;
+
+  Task({
+    required this.text,
+    required this.done,
+  });
+}
